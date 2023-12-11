490ffdf051b73976887d511b45023e18a5c35980
Martijn Boers <martijn@plebian.nl>
2023-12-11 18:00:15 +0100

    Get powerlevel10k working in zsh

--- home.nix
+++ home.nix
@@ -11,22 +11,44 @@
     enable = true;
     userName = "Martijn Boers";
     userEmail = "martijn@plebian.nl";
+    extraConfig = {
+      core.editor = "vim";
+      pull.rebase = "true";
+    };
   };
 
   # ZSH stuff
   programs.zsh = {
     enable = true;
     shellAliases = {
-      update = "cd ~/Nix && sudo nixos-rebuild switch --flake .#glassdoor";
+      update = "sudo nixos-rebuild switch --flake /home/martijn/Nix#glassdoor";
       dud = "docker-compose up -d";
       fixup = "ga . && gc --amend --no-edit";
     };
+    dotDir = ".config/zsh";
+    initExtra = ''
+      # Powerlevel10k Zsh theme
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh
+    '';
     oh-my-zsh = {
       enable = true;
-      plugins = ["git" "thefuck"];
-#      theme = "power10k";
+      plugins = ["git" "thefuck" "direnv" "fzf" "z"];
     };
   };
+
+  # KDE
+#  programs.plasma = {
+#    enable = true;
+#
+#    workspace = {
+#      clickItemTo = "select";
+#      tooltipDelay = 5;
+#      theme = "breeze-dark";
+#      colorscheme = "BreezeDark";
+#    };
+#  };
+
 
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
@@ -36,6 +58,7 @@
     firefox
     vim
     sublime-merge
+    joplin-desktop
 
     # shell
     zsh-powerlevel10k
@@ -55,6 +78,7 @@
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processer https://github.com/mikefarah/yq
     fzf # A command-line fuzzy finder
+    direnv # used for .envrc files
 
     # networking tools
     dnsutils # `dig` + `nslookup`
