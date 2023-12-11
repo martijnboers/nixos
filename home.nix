5ffcf49ed524bbf53673755cdd9e60532fa02be1
Martijn Boers <martijn@plebian.nl>
2023-12-11 18:02:51 +0100

    Add zoxide

--- home.nix
+++ home.nix
@@ -77,11 +77,14 @@
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processer https://github.com/mikefarah/yq
-    fzf # A command-line fuzzy finder
-    direnv # used for .envrc files
 
     # networking tools
     dnsutils # `dig` + `nslookup`
+
+    # terminal
+    zoxide
+    fzf # A command-line fuzzy finder
+    direnv # used for .envrc files
 
     # misc
     file
@@ -93,12 +96,6 @@
     zstd
     gnupg
     git
-
-    # nix related
-    #
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
-    nix-output-monitor
 
     htop
     btop # fancy htop
