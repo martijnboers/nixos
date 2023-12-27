851f03beb155e84d58f65c328e3fa757b131ca20
Martijn Boers <martijn@plebian.nl>
2023-12-27 12:27:38 +0100

    Add bootloader rules

--- hosts/testbed/default.nix
+++ hosts/testbed/default.nix
@@ -5,4 +5,8 @@
 }: {
   # QEMU virtual machine
   networking.hostName = "testbed";
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
 }
