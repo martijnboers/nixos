17fe3bf348a8af45aa6c781d7a1614179d50a071
Martijn Boers <martijn@plebian.nl>
2024-01-05 16:33:41 +0100

    Use nix-index home-manager module

--- home/modules/ranger.nix
+++ home/modules/ranger.nix
@@ -6,24 +6,11 @@
 }:
 with lib; let
   cfg = config.programs.ranger;
-  functions = {
-    r = {
-      body = ''
-        set NUM (pstree -s %self | grep -o ranger | wc -l)
-        if test $NUM -eq 0
-          ranger && history --merge
-        else
-          exit
-        end
-      '';
-    };
-  };
 in {
   meta.maintainers = [];
 
   options.programs.ranger = {
     enable = mkEnableOption "ranger, a vim-inspired filemanager for the console";
-    enableFishIntegration = mkEnableOption "enable r command in fish";
     package = mkPackageOption pkgs "ranger" {};
     extraConfig = mkOption {
       type = types.lines;
@@ -39,7 +26,6 @@ in {
 
   config = mkIf cfg.enable {
     home.packages = [cfg.package];
-    programs.fish.functions = mkIf cfg.enableFishIntegration functions;
     xdg.configFile."ranger/rc.conf" = mkIf (cfg.extraConfig != "") {text = cfg.extraConfig;};
   };
 }
