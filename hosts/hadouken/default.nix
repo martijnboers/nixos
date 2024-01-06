a35f108191f477ef16b8b1f717d75e3f0d3061bb
Martijn Boers <martijn@plebian.nl>
2024-01-06 12:54:56 +0100

    Add silent boot

--- hosts/hadouken/default.nix
+++ hosts/hadouken/default.nix
@@ -5,6 +5,31 @@
 }: {
   networking.hostName = "hadouken";
 
+  # Bootloader.
+  boot = {
+    loader = {
+      # Use the systemd-boot EFI boot loader.
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    # Silent Boot
+    # https://wiki.archlinux.org/title/Silent_boot
+    kernelParams = [
+      "quiet"
+      "splash"
+      "vga=current"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    consoleLogLevel = 0;
+    # https://github.com/NixOS/nixpkgs/pull/108294
+    initrd = {
+      verbose = false;
+    };
+  };
+
   # Enable ssh for host
   programs.openssh.enable = true;
 }
