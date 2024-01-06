2ddd3a529d4b72386c28eeb3bebc0a8887aa72c8
Martijn Boers <martijn@plebian.nl>
2024-01-06 13:39:30 +0100

    Change permissions samba share

--- hosts/glassdoor/default.nix
+++ hosts/glassdoor/default.nix
@@ -17,7 +17,7 @@
     options = let
         # this line prevents hanging on network split
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      in ["${automount_opts},credentials=${config.age.secrets.smb.path}"];
+      in ["${automount_opts},credentials=${config.age.secrets.smb.path},uid=1000,gid=100"];
   };
 
   # QEMU virtualization
