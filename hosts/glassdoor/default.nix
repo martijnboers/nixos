42289d4c9abc123dd956864d34dff262f07a03c9
Martijn Boers <martijn@plebian.nl>
2024-01-06 13:34:45 +0100

    Allow glassdoor to smb

--- hosts/glassdoor/default.nix
+++ hosts/glassdoor/default.nix
@@ -14,7 +14,10 @@
   fileSystems."/mnt/share" = {
     device = "//192.168.1.156/public";
     fsType = "cifs";
-    options = ["credentials=${config.age.secrets.smb.path},uid=1000,gid=100"];
+    options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in ["${automount_opts},credentials=${config.age.secrets.smb.path}"];
   };
 
   # QEMU virtualization
