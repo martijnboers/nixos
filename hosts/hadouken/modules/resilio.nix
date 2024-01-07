17a14ffddf817b7d9e01be12bde3921680a102bf
Martijn Boers <martijn@plebian.nl>
2024-01-07 18:35:34 +0100

    Add users statement outside of service statement

--- hosts/hadouken/modules/resilio.nix
+++ hosts/hadouken/modules/resilio.nix
@@ -12,11 +12,12 @@ in {
   };
 
   config = mkIf cfg.enable {
+    users.users.martijn.extraGroups = ["rslsync"];
+
     services.resilio = {
       enable = true;
       deviceName = "hadouken";
       enableWebUI = true;
-      users.users.martijn.extraGroups = ["rslsync"];
       httpLogin = "martijn";
       httpPass = builtins.readFile config.age.secrets.resilio.path;
       httpListenPort = "9001";
