676745638292e21aa9bc668bbdbd719feacda054
Martijn Boers <martijn@plebian.nl>
2024-01-07 16:34:14 +0100

    Remove testbed modules

--- hosts/testbed/default.nix
+++ hosts/testbed/default.nix
@@ -6,12 +6,6 @@
   # QEMU virtual machine
   networking.hostName = "testbed";
 
-  imports = [
-    ./modules/caddy.nix
-    ./modules/vaultwarden.nix
-  ];
-
-  hosts.caddy.enable = true;
   hosts.openssh.enable = true;
 
   boot.loader.grub.enable = true;
