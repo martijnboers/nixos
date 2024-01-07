04c59cd71c6efd1038b8a19dbc766739a88813ff
Martijn Boers <martijn@plebian.nl>
2024-01-07 17:34:17 +0100

    Add CoreDNS local domains

---
+++ hosts/hadouken/modules/coredns.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.hosts.coredns;
+in {
+  options.hosts.coredns = {
+    enable = mkEnableOption "CoreDNS server enable";
+  };
+
+  config = mkIf cfg.enable {
+    services.coredns.enable = true;
+
+    networking.firewall.allowedTCPPorts = [53];
+    networking.firewall.allowedUDPPorts = [53];
+
+    services.coredns.config = ''
+      .:53 {
+        hosts {
+          192.168.1.156  hadouken.plebian.local
+          192.168.1.114  glassdoor.plebian.local
+          192.168.1.1    router.plebian.local
+        }
+      }
+    '';
+  };
+}
