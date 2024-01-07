50763568c26e7a4569b036e6a94028bfe26ff7b2
Martijn Boers <martijn@plebian.nl>
2024-01-07 16:36:58 +0100

    Fix port number

--- hosts/hadouken/modules/caddy.nix
+++ hosts/hadouken/modules/caddy.nix
@@ -25,7 +25,7 @@ in {
         file_server
       '';
       virtualHosts."noisesfrom.space".extraConfig = ''
-        reverse_proxy http://localhost:${toString config.services.vaultwarden.config.ROCKET_PORT}
+        reverse_proxy http://localhost:${toString config.services.vaultwarden.config.rocketPort}
       '';
     };
   };
