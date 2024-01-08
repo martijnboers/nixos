d3dd8fddb8cbab28cb50626cb15d4b900b2de536
Martijn Boers <martijn@plebian.nl>
2024-01-08 23:43:52 +0100

    Mangle nextcloud to use Caddy

--- hosts/hadouken/modules/nextcloud.nix
+++ hosts/hadouken/modules/nextcloud.nix
@@ -12,11 +12,17 @@ in {
   };
 
   config = mkIf cfg.enable {
+    services.nginx.enable = false;
+    services.phpfpm.pools.nextcloud.settings = {
+      "listen.owner" = config.services.caddy.user;
+      "listen.group" = config.services.caddy.group;
+    };
+    # Needed to read /var/lib/nextcloud
+    users.groups.nextcloud.members = ["nextcloud" config.services.caddy.user];
+
     services.nextcloud = {
       enable = true;
       database.createLocally = true;
-      hostName = "next.plebian.nl";
-      https = true;
       config = {
         dbtype = "pgsql";
         adminuser = "admin";
