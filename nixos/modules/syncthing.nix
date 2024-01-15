960df7ed5c6f209442b10dcc316b5456e282cdbc
Martijn Boers <martijn@plebian.nl>
2024-01-15 21:35:49 +0100

    enable localAnnounceEnabled and remove home manager syncthing

--- nixos/modules/syncthing.nix
+++ nixos/modules/syncthing.nix
@@ -19,7 +19,8 @@ in {
   config = mkIf cfg.enable {
     services.syncthing = {
       enable = true;
-      guiAddress = "${cfg.ipaddress}:8384";
+      guiAddress = "127.0.0.1:8384";
+      openDefaultPorts = true;
       user = "martijn";
       dataDir = "/home/martijn/Sync"; # Default folder for new synced folders
       overrideDevices = true; # overrides any devices added or deleted through the WebUI
@@ -28,18 +29,24 @@ in {
         devices = {
           "glassdoor" = {
             id = "L77BOY3-HVS7OGS-6ABZ3T6-RDUSIB4-GZHNSCW-B5DVI3V-74JW4B6-T7B6PAS";
+            autoAcceptFolders = true;
+            allowedNetwork = "100.64.0.0/10";
             addresses = [
               "tcp://100.64.0.4:22000"
             ];
           };
           "phone" = {
             id = "4ROZWW2-EAWAQ3S-NQDS7HL-HHJU2PT-UJNRCRE-ZO5VKPN-CECNL6D-LEEYLQP";
+            autoAcceptFolders = true;
+            allowedNetwork = "100.64.0.0/10";
             addresses = [
               "tcp://100.64.0.3:22000"
             ];
           };
           "lapdance" = {
             id = "DZMLQOR-CGFZN2K-RATVUCL-PTTDLPO-FYEVRCB-3W2OARE-YFXJIGD-KGRZNAO";
+            autoAcceptFolders = true;
+            allowedNetwork = "100.64.0.0/10";
             addresses = [
               "tcp://100.64.0.1:22000"
             ];
@@ -52,7 +59,7 @@ in {
           };
         };
         options = {
-          localAnnounceEnabled = false;
+          localAnnounceEnabled = true;
           globalAnnounceEnabled = false;
           listenAddresses = [
             "tcp4://${cfg.ipaddress}:22000"
