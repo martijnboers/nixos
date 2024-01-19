b0489cf84c6155ffc9d5a27c5d7d6bd062048d5f
Martijn Boers <martijn@plebian.nl>
2024-01-19 23:58:16 +0100

    Add to allowed machines

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINg7xqRr7ZqBTBsNvjBlqQXBE+9K2/5Qn4MT7VxwVTQj testbed@plebian.nl";
   hadouken = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFaPlgRyXXEbbtzYgBPSkGWaY1vNvL/hV3CTJ1HOisv hadouken@plebian.nl";
   suydersee = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgSOvTnSWpOlBB2c69WFyVQubyKhUYy5FcuDlCN/Xqv suydersee@plebian.nl";
-  users = [martijn testbed hadouken];
+  users = [martijn testbed hadouken suydersee];
 in {
   "hosts.age".publicKeys = users;
   "password.age".publicKeys = users;
