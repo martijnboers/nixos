b3f628e076827a3db1a210a7ef7567f5a62a7fa7
Martijn Boers <martijn@plebian.nl>
2024-01-20 18:16:49 +0100

    Disable Whitelist

--- hosts/suydersee/modules/transmission.nix
+++ hosts/suydersee/modules/transmission.nix
@@ -21,9 +21,8 @@ in {
         settings = {
           download-dir = "/mnt/garage/Music";
           incomplete-dir-enabled = true;
-          rpc-whitelist = "127.0.0.1,192.168.*.*";
           rpc-bind-address = "0.0.0.0";
-          rpc-whitelist = "127.0.0.1,10.0.0.1"; #Whitelist your remote machine (10.0.0.1 in this example)
+#          rpc-whitelist = "127.0.0.1,10.0.0.1"; #Whitelist your remote machine (10.0.0.1 in this example)
         };
       };
     };
