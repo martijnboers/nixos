97696193ad60cc73cb44d616fd02ffa4523fc7d0
Martijn Boers <martijn@plebian.nl>
2024-01-20 19:56:16 +0100

    Run transmission on localhost

--- hosts/suydersee/modules/transmission.nix
+++ hosts/suydersee/modules/transmission.nix
@@ -14,15 +14,15 @@ in {
   config = mkIf cfg.enable {
     services = {
       caddy.virtualHosts."transmission.thuis.plebian.nl".extraConfig = ''
-        reverse_proxy http://100.64.0.5:${toString config.services.transmission.port}
+        reverse_proxy http://127.0.0.1:${toString config.services.transmission.settings.rpc-port}
       '';
       transmission = {
         enable = true; #Enable transmission daemon
-        openRPCPort = true;
+        openPeerPorts = true;
         settings = {
           download-dir = "/mnt/garage/Music";
           incomplete-dir-enabled = true;
-          rpc-bind-address = "100.64.0.5";
+          rpc-bind-address = "127.0.0.1";
         };
       };
     };
