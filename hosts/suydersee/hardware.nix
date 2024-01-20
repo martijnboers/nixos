c0d5f6cd3029178663098420ccbb6e871d67f9dc
Martijn Boers <martijn@plebian.nl>
2024-01-20 15:58:53 +0100

    Put default gateway in right spot

--- hosts/suydersee/hardware.nix
+++ hosts/suydersee/hardware.nix
@@ -34,7 +34,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.ens3 = {
+  networking = {
     interfaces = {
       ens3 = {
         ipv6.addresses = [
