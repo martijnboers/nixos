b4b2f2c5e477a14e9465c90ba3cc98112db83f57
Martijn Boers <martijn@plebian.nl>
2024-01-22 17:08:38 +0100

    Switch ca and key position

--- hosts/hadouken/modules/atuin.nix
+++ hosts/hadouken/modules/atuin.nix
@@ -13,7 +13,7 @@ in {
 
   config = mkIf cfg.enable {
     services.caddy.virtualHosts."atuin.thuis.plebian.nl".extraConfig = ''
-      tls ${config.age.secrets.key.path} ${config.age.secrets.ca.path}
+      tls ${config.age.secrets.ca.path} ${config.age.secrets.key.path}
       reverse_proxy http://localhost:${toString config.services.atuin.port}
     '';
     services.atuin = {
