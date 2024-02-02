4b09ed48c472874cf8d4da399eeee6b77c882e48
Martijn Boers <martijn@plebian.nl>
2024-02-02 10:31:16 +0100

    Enable silent boot lapdance

--- hosts/lapdance/default.nix
+++ hosts/lapdance/default.nix
@@ -32,8 +32,31 @@
   # Docker + QEMU
   hosts.virtualization.enable = true;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    # Bootloader.
+  boot = {
+    loader = {
+      # Use the systemd-boot EFI boot loader.
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    # Silent Boot
+    # https://wiki.archlinux.org/title/Silent_boot
+    kernelParams = [
+      "quiet"
+      "splash"
+      "vga=current"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    consoleLogLevel = 0;
+    # https://github.com/NixOS/nixpkgs/pull/108294
+    initrd = {
+      verbose = false;
+      kernelModules = ["amdgpu"];
+    };
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
