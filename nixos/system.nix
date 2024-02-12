28a7009f3947b27ea1ce2bfc97f623cf1b0e87cc
Martijn Boers <martijn@plebian.nl>
2024-02-12 14:50:49 +0100

    Complete nix-ld setup

--- nixos/system.nix
+++ nixos/system.nix
@@ -178,6 +178,21 @@
     };
   };
 
+  # Inject patched libraries to unpatched bins
+  programs.nix-ld.enable = true;
+  # Sets up all the libraries to load
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    fuse3
+    icu
+    zlib
+    nss
+    openssl
+    curl
+    expat
+  ];
+
   nixpkgs = {
     overlays = [
       outputs.overlays.additions
