e7f2028e0fb3f8008f2e16350c51134ad74dd64f
Martijn Boers <martijn@plebian.nl>
2024-03-29 14:58:37 +0100

    Disable sandbox only on hadouken

--- nixos/system.nix
+++ nixos/system.nix
@@ -105,10 +105,6 @@
   nix.settings ={
     experimental-features = ["nix-command" "flakes"];
     trusted-users = ["martijn"];
-
-    # Allow network access when building
-    # https://mdleom.com/blog/2021/12/27/caddy-plugins-nixos/#xcaddy
-    sandbox = false;
   };
 
   # Collect nix store garbage and optimise daily.
