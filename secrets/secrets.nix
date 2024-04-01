28bb64bea0c8e06854daeeaf5d823e860abbe3e8
Martijn Boers <martijn@plebian.nl>
2024-04-01 20:26:43 +0200

    Move old key to laptop

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,9 @@
 let
   martijn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA1ejrK2xtmts+pJj/2mPZHTj3HEXAznPKVJ/MhHa6PV martijn@ssh.thuis.plebian.nl";
   testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINg7xqRr7ZqBTBsNvjBlqQXBE+9K2/5Qn4MT7VxwVTQj testbed@plebian.nl";
+  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIElgHjsOqLVDjObBrhg3gCQO7nesudsepiJxoTkBYCEl martijn@plebian.nl";
   hadouken = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFaPlgRyXXEbbtzYgBPSkGWaY1vNvL/hV3CTJ1HOisv hadouken@plebian.nl";
-  users = [martijn testbed hadouken];
+  users = [martijn testbed hadouken laptop];
   hadoukens = [hadouken];
 in {
   "hosts.age".publicKeys = users;
