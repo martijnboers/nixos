ba4915773281420a80edfadbc2ee2bf8fd8bc1b4
Martijn Boers <martijn@plebian.nl>
2024-04-01 18:38:03 +0200

    Setup ACL for SSH tailscale

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,16 +3,18 @@ let
   testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINg7xqRr7ZqBTBsNvjBlqQXBE+9K2/5Qn4MT7VxwVTQj testbed@plebian.nl";
   hadouken = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFaPlgRyXXEbbtzYgBPSkGWaY1vNvL/hV3CTJ1HOisv hadouken@plebian.nl";
   users = [martijn testbed hadouken];
-  hadouken = [hadouken];
+  hadoukens = [hadouken];
 in {
   "hosts.age".publicKeys = users;
   "password.age".publicKeys = users;
   "smb.age".publicKeys = users;
   "openai.age".publicKeys = users;
-  "nextcloud.age".publicKeys = hadouken;
   "borg.age".publicKeys = users;
-  "pgrok.age".publicKeys = hadouken;
-  "keycloak.age".publicKeys = hadouken;
-  "caddy.age".publicKeys = hadouken;
-  "headscale.age".publicKeys = hadouken;
+
+  "nextcloud.age".publicKeys = hadoukens;
+  "acl.age".publicKeys = hadoukens;
+  "pgrok.age".publicKeys = hadoukens;
+  "keycloak.age".publicKeys = hadoukens;
+  "caddy.age".publicKeys = hadoukens;
+  "headscale.age".publicKeys = hadoukens;
 }
