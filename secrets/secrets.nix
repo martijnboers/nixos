32061f1d7c85158faeded6619cb16e45da7b8fa7
Martijn Boers <martijn@plebian.nl>
2024-04-01 23:01:14 +0200

    Read pub keys from files

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,8 @@
 let
-  martijn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA1ejrK2xtmts+pJj/2mPZHTj3HEXAznPKVJ/MhHa6PV martijn@ssh.thuis.plebian.nl";
+  martijn = (builtins.readFile ../nixos/keys/martijn.pub);
+  laptop = (builtins.readFile ../nixos/keys/laptop.pub);
+  hadouken = (builtins.readFile ../nixos/keys/hadouken.pub);
   testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINg7xqRr7ZqBTBsNvjBlqQXBE+9K2/5Qn4MT7VxwVTQj testbed@plebian.nl";
-  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIElgHjsOqLVDjObBrhg3gCQO7nesudsepiJxoTkBYCEl martijn@plebian.nl";
-  hadouken = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFaPlgRyXXEbbtzYgBPSkGWaY1vNvL/hV3CTJ1HOisv hadouken@plebian.nl";
   users = [martijn testbed hadouken laptop];
   hadoukens = [hadouken martijn];
 in {
