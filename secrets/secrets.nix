b29cde38639709c6bd902c56ac4adb5f6e82f069
Martijn Boers <martijn@plebian.nl>
2024-04-03 10:07:25 +0200

    Change to adhere to update

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
-  martijn = (builtins.readFile ../nixos/keys/martijn.pub);
-  laptop = (builtins.readFile ../nixos/keys/laptop.pub);
-  hadouken = (builtins.readFile ../nixos/keys/hadouken.pub);
+  martijn = builtins.readFile ../nixos/keys/martijn.pub;
+  laptop = builtins.readFile ../nixos/keys/laptop.pub;
+  hadouken = builtins.readFile ../nixos/keys/hadouken.pub;
   testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINg7xqRr7ZqBTBsNvjBlqQXBE+9K2/5Qn4MT7VxwVTQj testbed@plebian.nl";
   users = [martijn testbed hadouken laptop];
   hadoukens = [hadouken martijn];
