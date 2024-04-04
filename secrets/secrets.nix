d78b438b426b1680fecb175717111d29db65c442
Martijn Boers <martijn@plebian.nl>
2024-04-04 11:01:05 +0200

    Revert replaceStrings

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
-  glassdoor = builtins.replaceStrings {"\n" = "";} builtins.readFile ../nixos/keys/glassdoor.pub;
-  laptop = builtins.replaceStrings {"\n" = "";} builtins.readFile ../nixos/keys/laptop.pub;
-  hadouken = builtins.replaceStrings {"\n" = "";} builtins.readFile ../nixos/keys/hadouken.pub;
+  # See: https://github.com/ryantm/agenix/pull/249
+  glassdoor = builtins.readFile ../nixos/keys/glassdoor.pub;
+  laptop = builtins.readFile ../nixos/keys/laptop.pub;
+  hadouken = builtins.readFile ../nixos/keys/hadouken.pub;
   testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINg7xqRr7ZqBTBsNvjBlqQXBE+9K2/5Qn4MT7VxwVTQj testbed@plebian.nl";
   users = [glassdoor testbed hadouken laptop];
   hadoukens = [hadouken glassdoor];
