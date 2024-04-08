6735f7bdb09fbf653978104d8e9b9a00a4735312
Martijn Boers <martijn@plebian.nl>
2024-04-08 10:50:20 +0200

    Add vscode for work when EAP not available

--- home/profiles/work.nix
+++ home/profiles/work.nix
@@ -13,6 +13,7 @@ in {
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
+      vscode
       jetbrains.pycharm-community
       # https://www.jetbrains.com/webstorm/nextversion/
       (pkgs.unstable.jetbrains.webstorm.overrideAttrs {
