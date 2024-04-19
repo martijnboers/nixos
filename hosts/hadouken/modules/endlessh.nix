d9ef372329486543789c05aba99d3c35e08150e1
Martijn Boers <martijn@plebian.nl>
2024-04-20 00:07:09 +0200

    Change DatabaseDirectory to something endlessh-go can access

--- hosts/hadouken/modules/endlessh.nix
+++ hosts/hadouken/modules/endlessh.nix
@@ -23,6 +23,8 @@ in {
           ];
           AccountID = 1003091;
           LicenseKey = config.age.secrets.geoip.path;
+          # Only path accassible for the service
+          DatabaseDirectory = "/run/endlessh-go/var";
         };
       };
       endlessh-go = {
