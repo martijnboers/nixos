cf2d0b00703949ab3f233299d6cb8cf818a076e4
Martijn Boers <martijn@plebian.nl>
2024-04-22 19:51:05 +0200

    Create matrix federation

---
+++ hosts/hadouken/modules/conduit.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.hosts.conduit;
+in {
+  options.hosts.conduit = {
+    enable = mkEnableOption "Matrix chat federation";
+  };
+
+  config = mkIf cfg.enable {
+    caddy.virtualHosts."id.plebian.nl, id.plebian.nl:8448".extraConfig = ''
+      reverse_proxy /_matrix/* http://localhost:${toString config.services.matrix-conduit.settings.global.port}
+    '';
+
+    services.matrix-conduit = {
+      enable = true;
+      settings.global = {
+        server_name = "id.plebian.nl";
+        allow_check_for_updates = false;
+        allow_registration = true;
+      };
+    };
+  };
+}
