2fb46f038359c6747d6cc3c12271c3f32ae7110c
Martijn Boers <martijn@plebian.nl>
2024-04-29 22:27:33 +0200

    Do callPackage in module

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,4 @@
 {pkgs, ...}: {
   httpie-desktop = pkgs.callPackage ./httpie-desktop.nix {};
   adguard-exporter = pkgs.callPackage ./adguard-exporter.nix {};
-  xcaddy = pkgs.callPackage ./xcaddy.nix {
-    plugins = ["github.com/caddy-dns/cloudflare"];
-  };
 }
