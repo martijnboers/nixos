b9972d7fc751c91ea54f9248fdd9eb0da861a1eb
Martijn Boers <martijn@plebian.nl>
2024-05-13 11:33:44 +0200

    Don't use microvm
    Untill it support non-nixos systems

--- nixos/modules/virtualization.nix
+++ nixos/modules/virtualization.nix
@@ -9,10 +9,6 @@ with lib; let
 in {
   options.hosts.virtualization = {
     enable = mkEnableOption "Enable virtualization";
-    networkInterface = mkOption {
-      type = types.str;
-      description = "interface of host";
-    };
   };
 
   config = mkIf cfg.enable {
@@ -20,39 +16,6 @@ in {
     virtualisation.docker.enable = true;
     users.users.martijn.extraGroups = ["docker" "libvirtd" "libvirt"];
     environment.systemPackages = with pkgs; [quickemu];
-
-    networking.useNetworkd = true;
-    systemd.network.enable = true;
-
-    # Used by microvm
-    systemd.network.networks."10-lan" = {
-      matchConfig.Name = [cfg.networkInterface "vm-*"];
-      networkConfig = {
-        Bridge = "br0";
-      };
-    };
-    systemd.network.netdevs."br0" = {
-      netdevConfig = {
-        Name = "br0";
-        Kind = "bridge";
-      };
-    };
-    systemd.network.networks."10-lan-bridge" = {
-      matchConfig.Name = "br0";
-      networkConfig = {
-        Address = ["192.168.1.2/24" "2001:db8::a/64"];
-        Gateway = "192.168.1.1";
-        DNS = ["192.168.1.1"];
-        IPv6AcceptRA = true;
-      };
-      linkConfig.RequiredForOnline = "routable";
-    };
-    systemd.network.networks."19-docker" = {
-      matchConfig.Name = "veth*";
-      linkConfig = {
-        Unmanaged = true;
-      };
-    };
 
     # QEMU virtualization
     virtualisation = {
