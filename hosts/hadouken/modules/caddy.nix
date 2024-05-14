1fe3e18f5805cbd2047587dd54b7e5aeacf9ddff
Martijn Boers <martijn@plebian.nl>
2024-05-14 14:13:12 +0200

    Change website version

--- hosts/hadouken/modules/caddy.nix
+++ hosts/hadouken/modules/caddy.nix
@@ -8,7 +8,7 @@ with lib; let
   cfg = config.hosts.caddy;
   plebianRepo = builtins.fetchGit {
     url = "https://github.com/martijnboers/plebian.nl.git";
-    rev = "968121cafdaffc23d11eff2b81532ba292a6d65a";
+    rev = "0379c63f293ce84e88d211c9d9828df8b4862067";
   };
 in {
   options.hosts.caddy = {
