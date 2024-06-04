6779f6793b7fac0bdaf267430af2e8f0f3624f1f
Martijn Boers <martijn@plebian.nl>
2024-06-04 21:13:58 +0200

    Remove references to home-manager desktop

--- hosts/glassdoor/home.nix
+++ hosts/glassdoor/home.nix
@@ -8,7 +8,6 @@
   age.identityPaths = ["/home/martijn/.ssh/id_ed25519_age"];
 
   # Enable profiles
-  hosts.desktop.enable = true;
   hosts.kde.enable = true;
   hosts.personal.enable = true;
   hosts.work.enable = true;
--- hosts/lapdance/home.nix
+++ hosts/lapdance/home.nix
@@ -4,7 +4,6 @@
   ];
 
   # Enable profiles
-  hosts.desktop.enable = true;
   hosts.kde.enable = true;
   hosts.personal.enable = false;
   hosts.work.enable = true;
