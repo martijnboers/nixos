22eb1e2a3b787b76c614869509650077471a204f
Martijn Boers <martijn@plebian.nl>
2024-07-18 17:03:26 +0200

    Rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,21 +1,24 @@
 let
   glassdoor = builtins.readFile ../nixos/keys/glassdoor.pub;
   hadouken = builtins.readFile ../nixos/keys/hadouken.pub;
+  shoryuken = builtins.readFile ../nixos/keys/shoryuken.pub;
   testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINg7xqRr7ZqBTBsNvjBlqQXBE+9K2/5Qn4MT7VxwVTQj testbed@plebian.nl";
-  users = [glassdoor testbed hadouken];
-  hadoukens = [hadouken];
+  basic = [glassdoor testbed hadouken shoryuken];
+  hadoukens = [glassdoor hadouken];
+  shoryukens = [glassdoor shoryuken];
 in {
-  "password.age".publicKeys = users;
-  "smb.age".publicKeys = users;
-  "openai.age".publicKeys = users;
-  "borg.age".publicKeys = users;
+  "password.age".publicKeys = basic;
+  "smb.age".publicKeys = basic;
+  "openai.age".publicKeys = basic;
+  "borg.age".publicKeys = basic;
+
+  "keycloak.age".publicKeys = shoryukens;
+  "headscale.age".publicKeys = shoryukens;
 
   "nextcloud.age".publicKeys = hadoukens;
   "acl.age".publicKeys = hadoukens;
   "pgrok.age".publicKeys = hadoukens;
-  "keycloak.age".publicKeys = hadoukens;
   "caddy.age".publicKeys = hadoukens;
-  "headscale.age".publicKeys = hadoukens;
   "geoip.age".publicKeys = hadoukens;
   "immich.age".publicKeys = hadoukens;
   "adguard.age".publicKeys = hadoukens;
