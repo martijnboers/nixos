47ef4378a1a25cb77c9a51f609e9ec5e962e706b
Martijn Boers <martijn@plebian.nl>
2024-07-19 15:57:19 +0200

    Disable passwordless root

--- nixos/system.nix
+++ nixos/system.nix
@@ -116,8 +116,7 @@
         # Optional, retains environment variables while running commands
         # e.g. retains your NIX_PATH when applying your config
         keepEnv = true;
-#        persist = true; # Optional, only require password verification a single time
-        noPass = true; # TODO: temp for nixos-everwhere
+        persist = true; # Optional, only require password verification a single time
       }
     ];
     # Self signed ca for all the internal tailscale services
