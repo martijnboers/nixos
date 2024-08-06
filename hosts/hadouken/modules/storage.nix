36a5a5bd85f14c2e6df074da89ffbddd684169ca
Martijn Boers <martijn@plebian.nl>
2024-08-06 22:54:30 +0200

    Build zfs with email support

--- hosts/hadouken/modules/storage.nix
+++ hosts/hadouken/modules/storage.nix
@@ -41,21 +41,17 @@
     fsType = "zfs";
   };
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    zfsStable = pkgs.zfsStable.override {enableMail = true;};
+  };
+
   services.zfs = {
     autoScrub.enable = true;
     zed = {
-      enableMail = false;
+      enableMail = true;
       settings = {
-        ZED_DEBUG_LOG = "/tmp/zed.debug.log";
         ZED_EMAIL_ADDR = ["root"];
-        ZED_EMAIL_PROG = "${pkgs.msmtp}/bin/msmtp";
-        ZED_EMAIL_OPTS = "@ADDRESS@";
-
-        ZED_NOTIFY_INTERVAL_SECS = 3600;
         ZED_NOTIFY_VERBOSE = true;
-
-        ZED_USE_ENCLOSURE_LEDS = true;
-        ZED_SCRUB_AFTER_RESILVER = true;
       };
     };
   };
