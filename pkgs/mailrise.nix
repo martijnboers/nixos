b97f8327adf9309e71434dcece08a2bb35c75e4e
Martijn Boers <martijn@plebian.nl>
2024-10-03 16:20:15 +0200

    Create mailrise package with nix-init

--- pkgs/mailrise.nix
+++ pkgs/mailrise.nix
@@ -1,31 +1,47 @@
-{
-  lib,
-  fetchPypi,
-  python3,
+{ lib
+, python3
+, fetchPypi
 }:
-python3.pkgs.buildPythonPackage rec {
+
+python3.pkgs.buildPythonApplication rec {
   pname = "mailrise";
   version = "1.4.0";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-BKl5g4R9L5IrygMd9Vbi20iF2APpxSSfKxU25naPGTc=";
   };
 
-  doCheck = false;
-  propagatedBuildInputs = with python3.pkgs; [
-    pyyaml
-    aiosmtpd
-    apprise
-    packaging
-    setuptools # No module named 'pkg_resources'
+  nativeBuildInputs = [
+    python3.pkgs.setuptools
+    python3.pkgs.setuptools-scm
+    python3.pkgs.wheel
   ];
 
+  propagatedBuildInputs = with python3.pkgs; [
+    aiosmtpd
+    apprise
+    pyyaml
+  ];
+
+  passthru.optional-dependencies = with python3.pkgs; {
+    testing = [
+      pytest
+      pytest-asyncio
+      pytest-cov
+      setuptools
+      types-pyyaml
+    ];
+  };
+
+  pythonImportsCheck = [ "mailrise" ];
+
   meta = with lib; {
-    description = "An SMTP gateway for Apprise notifications.";
-    homepage = "https://github.com/YoRyan/mailrise";
+    description = "An SMTP gateway for Apprise notifications";
+    homepage = "https://pypi.org/project/mailrise/";
     license = licenses.mit;
-    maintainers = with maintainers; [];
+    maintainers = with maintainers; [ ];
     mainProgram = "mailrise";
   };
 }
